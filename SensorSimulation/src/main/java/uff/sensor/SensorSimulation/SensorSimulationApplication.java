341b8bcb35ce577c318a13afe6a13c48122958ac
Paulo Henrique Tavares Borges <paulo.borges@radixeng.com.br>
2018-09-30 10:48:20 -0300

    start repo

---
+++ SensorSimulation/src/main/java/uff/sensor/SensorSimulation/SensorSimulationApplication.java
@@ -0,0 +1,12 @@
+package uff.sensor.SensorSimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorSimulationApplication.class, args);
+	}
+}
